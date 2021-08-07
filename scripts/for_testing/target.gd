4d57a3137e41b4ace0de071b0ccbd1999d9ab711
gone-noir <number1xman@gmail.com>
2021-08-07 15:17:24 -0500

    Fixed dumb mistakes

--- scripts/for_testing/target.gd
+++ scripts/for_testing/target.gd
@@ -1,14 +1,16 @@
 extends Sprite
 
-#func _ready():
-#	get_parent().target = self
+onready var tween: = $Tween as Tween
 
 func _physics_process(delta):
 	global_position = get_global_mouse_position()
 
 func hit():
-	pass
+	tween.interpolate_property(	self, "modulate", Color.red, Color.white, .5, 
+								Tween.TRANS_QUART, Tween.EASE_OUT)
+	tween.start()
 
 func _on_area_shape_entered(area_id, area, area_shape, local_shape):
 	hit()
+#	area.all_bullets[area_shape].set_physics_process(false)
 	area.reallocate_bullet(area_shape)
